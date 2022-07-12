32a7eb4d60464b55ebe5c43fbe8ca61c323d8c82
Vivek459 <vivek.1923en1091@kiet.edu>
2022-07-12 09:24:56 +0530

    Create Squares_of_a_Sorted_Array.java

---
+++ ALGO I/Day 2/Squares_of_a_Sorted_Array.java
@@ -0,0 +1,29 @@
+class Solution {
+
+    public int[] sortedSquares(int[] A) {
+     
+        if (A == null) return null;
+         
+        int [] ans = new int[A.length];
+      
+        int start = 0, end = A.length - 1;
+        
+        int i = end;
+        
+        while (start <= end) { 
+
+            int pow1 = A[start] * A[start];
+            int pow2 = A[end] * A[end];
+            
+            if (pow1 > pow2) {
+                ans[i--] = pow1;
+                start++;
+            } else {
+                ans[i--] = pow2;
+                end--;
+            }
+        }
+        
+        return ans;
+    }
+}
